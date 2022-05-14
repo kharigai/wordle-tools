0c807dbd4dfc44be32b3873af3d0f6d31d85117f
Kazuhiro Harigai <kharigai@gmail.com>
2022-05-14 23:21:30 +0900

    -i, --ignore option を追加しました。

--- five-letter-word.py
+++ five-letter-word.py
@@ -11,14 +11,22 @@ def get_five_letter_words(fn: str) -> List[str]:
     except:
         return []
 
+def is_exit_ignore_letter(word: str, ignore_letters: List[str]) -> bool:
+     return len([l for l in list(word) if l in ignore_letters]) > 0
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-r', '--random', action='store_true', dest='show_random', default=False)
     parser.add_option('-f', '--file', dest='fnm_word_list', default='./word.list')
+    parser.add_option('-i', '--ignore', dest='ignore_letters', default='')
     parser.parse_args()
     options, args = parser.parse_args()
 
+    ignore_letters = [l for l in options.ignore_letters.split(',') if l != '']
+
     words = get_five_letter_words(options.fnm_word_list)
+    if len(words) > 0 and len(ignore_letters) > 0:
+        words = [w for w in words if not is_exit_ignore_letter(w, ignore_letters)]
     if options.show_random:
         print(words[randint(1, len(words))])
     else:
